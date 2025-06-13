adddf5cd99908aa29b64c949eee42f611be15a2c
Getxeza <Getxe15@gmail.com>
2025-06-13 11:58:19 +0700

    update file helloworld and variables

---
+++ src/com/project/java_basic/TypeVariables.java
@@ -0,0 +1,21 @@
+package com.project.java_basic;
+
+public class TypeVariables {
+    public static void main(String[] args) {
+        String name = "Getxe" ; //ประกาศตัวแปรแบบข้อความ
+        System.out.println(name);
+
+        int mumber = 15;
+        System.out.println(mumber);
+        double num = 15.99 ;
+        char A = 'b';
+        boolean result = false;
+        // ture แปลว่า จริง
+        // False แปลว่า เท็จ
+
+        System.out.println(A+num+"Getxe");
+
+    }
+
+
+}
