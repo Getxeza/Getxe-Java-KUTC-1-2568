0b783c3c1f5632912789d1d2a20e66c418e8b003
Getxeza <Getxe15@gmail.com>
2025-06-20 12:03:43 +0700

    update file helloworld and variables

---
+++ src/com/project/java_basic/MathBasic.java
@@ -0,0 +1,50 @@
+package com.project.java_basic;
+
+public class MathBasic {
+    public static void main(String[] args) {
+//        int nam_1  = 10;
+//        int num_2  = 3;
+//        int result ;
+//
+//        result = nam_1+num_2 ;
+//
+//        result = (double) nam_1 / num_2 ;
+//
+//        result = nam_1 % num_2 ; การหารแบบเอาเศษใช้เครื่องหมาย %
+//
+//        System.out.println(result);
+
+
+     int total = 10+20*(2+5) ;
+     System.out.println(total);
+
+//     BMI = น้ำหนัก / (ส่วนสูง x ส่วนสูง )
+
+        int wight = 60 ;
+        double hight = 160 ;
+        double BMI;
+
+//        1 เมตร = 100 เซนติเมตร
+         BMI = wight / ((hight/100)*(hight/100)) ;
+         System.out.println(BMI);
+
+
+
+         int Byte = 10000000 ;
+         int Mb ;
+
+         Mb = Byte / 1048576;
+         System.out.println(Mb);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
